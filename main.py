f93046f2a63e11945807074960e51f6ec9ce69c1
zoichhh31 <mykyhliaaa31@gmail.com>
2024-06-11 17:44:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hi world!")
+meme_dict = {
+            "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
+            "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
+            "ROFL": "Tanggapan terhadap lelucon",
+            "SHEESH": "Sedikit ketidaksetujuan",
+            "CREEPY": "Menakutkan, tidak menyenangkan",
+            "AGGRO": "Untuk menjadi agresif/marah",
+            }
+word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ").upper()
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("Kata itu tidak ada di dalam kamus")
